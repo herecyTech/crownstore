5dc3cd512de744e5b2c3646b8590ffb3941e9a95
herecyTech <thundercallsustoact@gmail.com>
2020-06-02 10:38:48 +0200

    finishing sign in handleSubmit

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.util';
+import { auth, signInWithGoogle } from '../../firebase/firebase.util';
 import './sign-in.styles.scss';
 
 class SignIn extends Component {
@@ -15,10 +15,16 @@ class SignIn extends Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-
-        this.setState({ email:'', password:''})
+        const { email, password } = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email:'', password:''})
+        } catch (error){
+            console.log(error);
+        }
+       
     }
     
     handleChange = event => {
