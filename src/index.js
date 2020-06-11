32342bc622af43e7a324ccb73f92b67dff6a3a8f
herecyTech <thundercallsustoact@gmail.com>
2020-06-11 13:14:53 +0200

    created shop reducer, moved shop data to reducer, implemented in shop component

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './index.css';
 import App from './App';
 
-import store from './components/redux/store';
+import {store, persistor } from './components/redux/store';
 
 ReactDOM.render(
  <Provider store={store}>
  <BrowserRouter>
-   <React.StrictMode>
+   <PersistGate persistor={persistor}>
+   
      <App />
-   </React.StrictMode>
+   
+   </PersistGate>
   </BrowserRouter>
   </Provider>,
   document.getElementById('root')
